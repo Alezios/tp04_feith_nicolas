b643693c94b91b641a62616da22318853d247e16
Nicolas F <nicolas.feith@gmail.com>
2021-11-04 14:26:47 +0100

    reimport and modif

--- FRONTEND/karma.conf.js
+++ FRONTEND/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tp4'),
+      dir: require('path').join(__dirname, './coverage/tp02-met02'),
       subdir: '.',
       reporters: [
         { type: 'html' },
